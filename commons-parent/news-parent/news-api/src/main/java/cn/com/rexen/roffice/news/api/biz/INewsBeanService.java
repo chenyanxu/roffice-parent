fe3db15d6a8e7d846996fed8955245799bf01d7a
sunlf <hanbi918@163.com>
2015-10-29 09:43:00 +0800

    增加代码生成模块，增加news模块

---
+++ commons-parent/news-parent/news-api/src/main/java/cn/com/rexen/roffice/news/api/biz/INewsBeanService.java
@@ -0,0 +1,16 @@
+package cn.com.rexen.roffice.news.api.biz;
+
+import cn.com.rexen.core.api.biz.IBizService;
+import cn.com.rexen.roffice.news.entities.NewsBean;
+
+/**
+ * @类描述：应用服务接口.
+ * @创建人：
+ * @创建时间：
+ * @修改人：
+ * @修改时间：
+ * @修改备注：
+ */
+public interface INewsBeanService extends IBizService<NewsBean> {
+    //在此添加新的业务方法
+}
