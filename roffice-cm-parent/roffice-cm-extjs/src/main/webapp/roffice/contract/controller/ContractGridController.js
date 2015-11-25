d3863ce24c277f728d6493193d7e708b3a2e198f
chenyanxu-rexen <chenyanxu@rexen.com.cn>
2015-11-25 17:37:57 +0800

    test for file uoload in contract

--- roffice-cm-parent/roffice-cm-extjs/src/main/webapp/roffice/contract/controller/ContractGridController.js
+++ roffice-cm-parent/roffice-cm-extjs/src/main/webapp/roffice/contract/controller/ContractGridController.js
@@ -3,6 +3,15 @@
  */
 Ext.define('kalix.roffice.contract.controller.ContractGridController', {
     extend: 'kalix.controller.BaseGridController',
-    alias: 'controller.contractGridController'
+    alias: 'controller.contractGridController',
     //add your own callback handler here
+    onAttachmentManage:function(grid, rowIndex, colIndex){
+        var view=Ext.create('kalix.attachment.view.AttachmentWindow');
+        var selModel = grid.getStore().getData().items[rowIndex];
+        var vm = view.lookupViewModel();
+
+        vm.set('rec', selModel);
+        view.show();
+        grid.setSelection(selModel);
+    }
 });
--- roffice-cm-parent/roffice-cm-extjs/src/main/webapp/roffice/contract/view/ContractGrid.js
+++ roffice-cm-parent/roffice-cm-extjs/src/main/webapp/roffice/contract/view/ContractGrid.js
@@ -20,52 +20,82 @@ Ext.define('kalix.roffice.contract.view.ContractGrid', {
     store: {
         type: 'contractStore'
     },
-    columns:{
-        defaults:{flex:1},
-        items:[
-        {
-            xtype: "rownumberer",
-            text: "行号",
-            width: 50,
-            flex:0,
-            align: 'center'
-        },
-        {
-            text: '编号',
-            dataIndex: 'id',
-            hidden: true
-        },
-        {
-            text: '合同编号',
-            dataIndex: 'contractNumber'
-        },
-        {
-            text: '项目名称',
-            dataIndex: 'projectName'
-        }, {
-            text: '甲方',
-            dataIndex: 'partyA'
-        }, {
-            text: '乙方',
-            dataIndex: 'partyB'
-        },
-        {
-            text: '合同状态',
-            xtype: 'dictGridColumn',
-            dictType: 'contractStatus',
-            dataIndex: 'contractStatus'
-        }
-        ,
-        {
-            xtype: 'securityGridColumnRUD',
-            //iconPath:'/kalix/roffice/cm/resources/images/contract',
-            permissions: [
-                'roffice:cmModule:contractMenu:view',
-                'roffice:cmModule:contractMenu:edit',
-                'roffice:cmModule:contractMenu:delete'
-            ]
-        }
-    ]},
+    columns: {
+        defaults: {flex: 1},
+        items: [
+            {
+                xtype: "rownumberer",
+                text: "行号",
+                width: 50,
+                flex: 0,
+                align: 'center'
+            },
+            {
+                text: '编号',
+                dataIndex: 'id',
+                hidden: true
+            },
+            {
+                text: '合同编号',
+                dataIndex: 'contractNumber'
+            },
+            {
+                text: '项目名称',
+                dataIndex: 'projectName'
+            }, {
+                text: '甲方',
+                dataIndex: 'partyA'
+            }, {
+                text: '乙方',
+                dataIndex: 'partyB'
+            },
+            {
+                text: '合同状态',
+                xtype: 'dictGridColumn',
+                dictType: 'contractStatus',
+                dataIndex: 'contractStatus'
+            }
+            ,
+            {
+                xtype: 'securityGridColumnCommon',
+                items: [
+                    {
+                        icon: "resources/images/read.png",
+                        permission: 'roffice:cmModule:contractMenu:view',
+                        tooltip: '查看',
+                        handler: 'onView'
+                    },
+                    {
+                        icon: "resources/images/edit.png",
+                        permission: 'roffice:cmModule:contractMenu:edit',
+                        tooltip: '编辑',
+                        handler: 'onEdit'
+                    }, {
+                        icon: "resources/images/delete.png",
+                        permission: 'roffice:cmModule:contractMenu:delete',
+                        tooltip: '删除',
+                        handler: 'onDelete'
+                    },
+                    {
+                        icon: "attachment/resources/images/attachment_manage.png",
+                        permission: 'roffice:cmModule:contractMenu:delete',
+                        tooltip: '附件管理',
+                        handler: 'onAttachmentManage'
+                    }
+                ]
+            }
+            //,
+            //{
+            //    xtype: 'securityGridColumnRUD',
+            //    //iconPath:'/kalix/roffice/cm/resources/images/contract',
+            //    permissions: [
+            //        'roffice:cmModule:contractMenu:view',
+            //        'roffice:cmModule:contractMenu:edit',
+            //        'roffice:cmModule:contractMenu:delete'
+            //    ]
+            //}
+        ]
+    },
     tbar: {
         xtype: 'securityToolbar',
         verifyItems: [
