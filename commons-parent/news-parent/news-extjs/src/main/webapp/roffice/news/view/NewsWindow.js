e4db8e941d723880ea88d5168226d33acf21c070
sunlf <hanbi918@163.com>
2015-12-04 21:21:51 +0800

    add tinyMce editor of news module

--- commons-parent/news-parent/news-extjs/src/main/webapp/roffice/news/view/NewsWindow.js
+++ commons-parent/news-parent/news-extjs/src/main/webapp/roffice/news/view/NewsWindow.js
@@ -9,7 +9,8 @@ Ext.define('kalix.roffice.news.view.NewsWindow', {
     requires: [
         'kalix.roffice.news.viewModel.NewsViewModel',
         'kalix.controller.BaseWindowController',
-        'kalix.admin.user.store.UserStore'
+        'kalix.admin.user.store.UserStore',
+        'Ext.ux.form.TinyMCETextArea'
     ],
     alias: 'widget.newsWindow',
     viewModel: 'newsViewModel',
@@ -18,7 +19,12 @@ Ext.define('kalix.roffice.news.view.NewsWindow', {
         storeId: 'newsStore'
     },
     xtype: "newsWindow",
-    width: 400,
+    width: 950,
+    height: 500,
+    layout: 'fit',
+    focusOnToFront: false,
+    toFrontOnShow: false,
+    preventFocusOnActivate: true,
     items: [{
         xtype: 'baseForm',
         items: [{
@@ -33,7 +39,25 @@ Ext.define('kalix.roffice.news.view.NewsWindow', {
             fieldLabel: '内容',
             labelAlign: 'right',
             allowBlank: false,
-            xtype: 'textarea',
+            //region: 'center',
+            xtype: 'tinymce_textarea',
+            height: 320,
+            //fieldStyle: 'font-family: Courier New; font-size: 12px;',
+            tinyMCEConfig: {
+                /*plugins: [
+                    "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+                    "searchreplace wordcount visualblocks visualchars code fullscreen",
+                    "insertdatetime media nonbreaking save table contextmenu directionality",
+                    "emoticons template paste textcolor"
+                ],*/
+                /*toolbar1: "newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
+                toolbar2: "cut copy paste | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | inserttime preview | forecolor backcolor",
+                toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft",
+                */content_css : "resources/css/contents.css",
+                menubar: true,
+                toolbar_items_size: 'small',
+                language:'zh_CN'
+            },
             bind: {
                 activeError: '{validation.content}',
                 value: '{rec.content}'
