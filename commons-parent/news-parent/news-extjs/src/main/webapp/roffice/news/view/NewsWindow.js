a9398773b545f9e537f6e30bd9be9db04280dcbe
chenyanxu-rexen <chenyanxu@rexen.com.cn>
2015-12-06 00:36:23 +0800

    fix news TinyMCETextArea bug

--- commons-parent/news-parent/news-extjs/src/main/webapp/roffice/news/view/NewsWindow.js
+++ commons-parent/news-parent/news-extjs/src/main/webapp/roffice/news/view/NewsWindow.js
@@ -42,7 +42,7 @@ Ext.define('kalix.roffice.news.view.NewsWindow', {
             //region: 'center',
             xtype: 'tinymce_textarea',
             height: 320,
-            //fieldStyle: 'font-family: Courier New; font-size: 12px;',
+            fieldStyle: 'font-family: Courier New; font-size: 12px;',
             tinyMCEConfig: {
                 /*plugins: [
                     "advlist autolink lists link image charmap print preview hr anchor pagebreak",
@@ -53,14 +53,20 @@ Ext.define('kalix.roffice.news.view.NewsWindow', {
                 /*toolbar1: "newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
                 toolbar2: "cut copy paste | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | inserttime preview | forecolor backcolor",
                 toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft",
-                */content_css : "resources/css/contents.css",
+                */
+                content_css : "resources/css/contents.css",
                 menubar: true,
                 toolbar_items_size: 'small',
                 language:'zh_CN'
             },
-            bind: {
-                activeError: '{validation.content}',
-                value: '{rec.content}'
+            listeners: {
+                change: function (field, newValue, oldValue) {
+                    console.log('change: ' + oldValue + ' -> ' + newValue);
+                    this.findParentByType('window').viewModel.get('rec').set('content',newValue);
+                },
+                beforerender:function(){
+                    this.setValue(this.findParentByType('window').viewModel.get('rec').get('content'));
+                }
             }
         }]
     }]
