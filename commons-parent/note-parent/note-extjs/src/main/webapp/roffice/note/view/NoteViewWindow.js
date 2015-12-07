c4c66e84fb6d91b141588b395d26b3b2bc0bdbf8
sunlingfeng-rexen <hanbi918@163.com>
2015-12-07 11:46:07 +0800

    新闻和公告修改成富文本。

--- commons-parent/note-parent/note-extjs/src/main/webapp/roffice/note/view/NoteViewWindow.js
+++ commons-parent/note-parent/note-extjs/src/main/webapp/roffice/note/view/NoteViewWindow.js
@@ -9,12 +9,14 @@ Ext.define('kalix.roffice.note.view.NoteViewWindow', {
     extend: 'kalix.view.components.common.BaseWindow',
     requires: [
         'kalix.roffice.note.viewModel.NoteViewModel',
-        'kalix.admin.user.store.UserStore'
+        'kalix.admin.user.store.UserStore',
+        'Ext.ux.form.TinyMCETextArea'
     ],
     alias: 'widget.noteViewWindow',
     viewModel: 'noteViewModel',
     xtype: "noteViewWindow",
-    width: 400,
+    width: 950,
+    height: 520,
     items: [{
         defaults: {readOnly: true},
         xtype: 'form',
@@ -42,8 +44,34 @@ Ext.define('kalix.roffice.note.view.NoteViewWindow', {
             }, {
                 fieldLabel: '内容',
                 labelAlign: 'right',
-                bind: {
-                    value: '{rec.content}'
+                allowBlank: false,
+                //region: 'center',
+                xtype: 'tinymce_textarea',
+                height: 320,
+                fieldStyle: 'font-family: Courier New; font-size: 12px;',
+                tinyMCEConfig: {
+                    plugins: [
+                        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+                        "searchreplace wordcount visualblocks visualchars code fullscreen",
+                        "insertdatetime media nonbreaking save table contextmenu directionality",
+                        "emoticons template paste textcolor"
+                    ],
+                    toolbar1: "newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
+                    toolbar2: "cut copy paste | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | inserttime preview | forecolor backcolor",
+                    toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft",
+                    content_css : "resources/css/contents.css",
+                    menubar: true,
+                    toolbar_items_size: 'small',
+                    language:'zh_CN'
+                },
+                listeners: {
+                    change: function (field, newValue, oldValue) {
+                        console.log('change: ' + oldValue + ' -> ' + newValue);
+                        this.findParentByType('window').viewModel.get('rec').set('content',newValue);
+                    },
+                    beforerender:function(){
+                        this.setValue(this.findParentByType('window').viewModel.get('rec').get('content'));
+                    }
                 }
             }, {
                 fieldLabel: '发布人',
