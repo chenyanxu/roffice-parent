1fb47fd5dbc8a95f024d147c6907c3bea095a8cf
chenyanxu <yanxu2016>
2016-01-08 10:07:38 +0800

    move the onAttachementManage function from child grid controller to the base grid controller

--- roffice-cm-parent/roffice-cm-extjs/src/main/webapp/roffice/contract/controller/ContractGridController.js
+++ roffice-cm-parent/roffice-cm-extjs/src/main/webapp/roffice/contract/controller/ContractGridController.js
@@ -5,15 +5,6 @@ Ext.define('kalix.roffice.contract.controller.ContractGridController', {
     extend: 'kalix.controller.BaseGridController',
     alias: 'controller.contractGridController',
     //add your own callback handler here
-    onAttachmentManage:function(grid, rowIndex, colIndex){
-        var view=Ext.create('kalix.attachment.view.AttachmentWindow');
-        var selModel = grid.getStore().getData().items[rowIndex];
-        var vm = view.lookupViewModel();
-
-        vm.set('rec', selModel);
-        view.show();
-        grid.setSelection(selModel);
-    }
     //,
     //onChange: function (target, event, domValue) {
     //    var form = target.findParentByType('form');
